7211f975e78ac4c7e9d028eac15e1bfd23fbc843
monica.pigrau <monibixv2@gmail.com>
2025-03-04 06:45:32 +0100

    EthWallet

--- EthWallet.sol
+++ EthWallet.sol
@@ -6,8 +6,11 @@ error NotEnoughFunds(address, uint256);
 
 contract EthWallet {
 
+    uint256 public constant FEE = 1; //1% Fixed transaction fee
+
     address public owner;
     mapping(address => uint256) private balances;
+    uint256 public fees;
 
     constructor() {
         owner = msg.sender;
@@ -18,14 +21,15 @@ contract EthWallet {
         _;
     }
 
+    // Allows the contract to receive ETH without calling a specific function
     receive() external payable {}
 
-    // User can deposit ETH to the smart contract
+    // Allows users to deposit ETH into the smart contract
     function deposit() public payable {
         balances[msg.sender] += msg.value;
     }
 
-    // User can withdraw maximum balance they have sent
+    // Allow users to withdraw up to their balance
     function withdraw(uint256 amount) public {
         if (balances[msg.sender] <= amount) revert NotEnoughFunds(msg.sender, amount);
  
@@ -34,24 +38,40 @@ contract EthWallet {
         require(sent, "Transfer failed");
     }
 
-    // User can transfer ETH to other accounts if they have enough funds
+    // Allow user transfer ETH between accounts with a 1% fee commission
     function transfer(uint256 amount, address payable to) public {
         require(amount <= balances[msg.sender], "Not enough funds");
 
+        uint256 fee_ = calculateFee(amount);
+        uint256 amountFee_ = amount - fee_; 
+
+        fees += fee_;
         balances[msg.sender] -= amount;
         balances[to] += amount;
-        (bool sent,) = to.call{value: amount}(""); 
+        (bool sent,) = to.call{value: amountFee_}(""); 
         require(sent, "Transfer failed");
     }
 
-    // User can get their balance
+    // Allows users to check their balances
     function getYourBalance() external view returns(uint256) {
         return balances[msg.sender];
     }
 
-    // Owner can get the balance of any account
-    function getAnyBalance(address wallet) public view onlyOwner returns(uint256) {
+    // Allows the owner to check the balance of any account
+    function getAnyBalance(address wallet) external view onlyOwner returns(uint256) {
         return balances[wallet];
+    }
+
+    // Allows the owner to withdraw the accumulated fees
+    function withdrawFees() external onlyOwner {
+        balances[owner] += fees;
+        fees = 0;
+        (bool sent, ) = owner.call{value: fees}("");
+        require(sent, "Transfer failed");
+    }
+
+    function calculateFee(uint256 amount) private pure returns(uint256) {
+        return (amount * FEE) / 100;
     }
 
 }
